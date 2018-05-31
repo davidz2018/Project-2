d4e93acc0c53c774dedd915c30c6a77779bea575
davidz2018 <dzhomebiz@gmail.com>
2018-05-30 18:00:19 -0700

    server update

--- server.js
+++ server.js
@@ -10,13 +10,10 @@ var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 var mysql = require('mysql');
 
-<<<<<<< HEAD
 var db = require("./models");
 var PORT = process.env.PORT || 8080;
 
 //routes 
-=======
->>>>>>> 3879dbe1042641214e49bddc406b9327315c597d
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
